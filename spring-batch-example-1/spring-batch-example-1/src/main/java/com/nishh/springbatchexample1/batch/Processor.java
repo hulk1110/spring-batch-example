fedbd6ca6affb6109fb1883544c4a8c801180c5d
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-09-24 20:47:21 +0530

    Processor Implementation

---
+++ spring-batch-example-1/spring-batch-example-1/src/main/java/com/nishh/springbatchexample1/batch/Processor.java
@@ -0,0 +1,32 @@
+package com.nishh.springbatchexample1.batch;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.nishh.springbatchexample1.model.User;
+
+public class Processor implements ItemProcessor<User, User>{
+	
+	
+	private static final Map<String,String> DEPT_NAME=
+			new HashMap<>();
+	
+	
+	public Processor() {
+		DEPT_NAME.put("001", "FSD2");
+		DEPT_NAME.put("002", "SD");
+		DEPT_NAME.put("003", "FSD");
+	}
+
+	@Override
+	public User process(User user) throws Exception {
+     
+		String deptCode =user.getDept();
+		String dept = DEPT_NAME.get(deptCode);
+		user.setDept(dept);
+         
+		return user;
+	}
+}
